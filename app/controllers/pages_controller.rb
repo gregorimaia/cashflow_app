1a6b799ef10f1d272eede1c7176280ba7f6d5a26
Gregori Maia <gregorimaia@gmail.com>
2023-09-08 08:41:35 -0300

    Adição da página controle

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
 	def home 
 	end
+
+	def controle
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  get 'controle', to: 'pages#controle'
 end
