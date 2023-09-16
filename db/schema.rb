5beb4b7c7d8ea6909a4f9cb1aa858a38f6659ca2
Gregori Maia <gregorimaia@gmail.com>
2023-09-16 06:19:29 -0300

    Adding links between pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-#ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 0) do
 
- # create_table "articles", force: :cascade do |t|
-  #  t.string "title"
-   # t.text "description"
-  #end
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+  end
   
-#end
+end
